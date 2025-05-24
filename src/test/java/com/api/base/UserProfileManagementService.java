89c3f9e738a6ec0929a69bde89b1c16a81b099f8
tejdeepch <tej.chinthamalla@gmail.com>
2025-05-24 12:52:11 +0530

    commited changes

--- src/test/java/com/api/base/UserProfileManagementService.java
+++ src/test/java/com/api/base/UserProfileManagementService.java
@@ -11,6 +11,7 @@ public static final String BASE_PATH="/api/users";
     }
     public Response updateProfile(String token, ProfileRequest payload){
         setAuthToken(token);
+
         return putRequest(payload, BASE_PATH+ "/profile");
     }
 }
